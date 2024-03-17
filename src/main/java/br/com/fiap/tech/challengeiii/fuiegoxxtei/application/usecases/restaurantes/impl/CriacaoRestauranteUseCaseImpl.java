befb586fe1a3fb50fe109e6296cdd127cfb77d70
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2024-03-16 23:03:17 -0300

    :construction: - Criar horários disponíveis por mesa

--- src/main/java/br/com/fiap/tech/challengeiii/fuiegoxxtei/application/usecases/restaurantes/impl/CriacaoRestauranteUseCaseImpl.java
+++ src/main/java/br/com/fiap/tech/challengeiii/fuiegoxxtei/application/usecases/restaurantes/impl/CriacaoRestauranteUseCaseImpl.java
@@ -2,7 +2,6 @@ package br.com.fiap.tech.challengeiii.fuiegoxxtei.application.usecases.restauran
 
 import br.com.fiap.tech.challengeiii.fuiegoxxtei.application.gateways.restaurante.CriacaoRestauranteGateway;
 import br.com.fiap.tech.challengeiii.fuiegoxxtei.application.usecases.restaurantes.CriacaoRestauranteUseCase;
-import br.com.fiap.tech.challengeiii.fuiegoxxtei.domain.exceptions.ApplicationException;
 import br.com.fiap.tech.challengeiii.fuiegoxxtei.presentation.dtos.restaurante.request.CriacaoRestauranteRequestDTO;
 import br.com.fiap.tech.challengeiii.fuiegoxxtei.presentation.dtos.restaurante.response.CriacaoRestauranteResponseDTO;
 import br.com.fiap.tech.challengeiii.fuiegoxxtei.presentation.mapper.restaurantes.CriacaoRestauranteMapper;
@@ -19,9 +18,9 @@ public class CriacaoRestauranteUseCaseImpl implements CriacaoRestauranteUseCase 
         var restaurante = this.mapper.criacaoRestauranteToRestaurante(request);
         restaurante.setTipoCozinha(request.tipoCozinhaEnum().getDescricao());
 
-        if (restaurante.getHrFimAtendimento().isBefore(restaurante.getHrInicioAtendimento())) {
-            throw new ApplicationException("A hora de fechamento do estabelecimento não pode ser menor que a data de abertura");
-        }
+//        if (restaurante.getHrFimAtendimento().isBefore(restaurante.getHrInicioAtendimento())) {
+//            throw new ApplicationException("A hora de fechamento do estabelecimento não pode ser menor que a data de abertura");
+//        }
 
         var restauranteDb = this.gateway.salvar(restaurante);
         return new CriacaoRestauranteResponseDTO(restauranteDb.getId());
---
+++ src/main/java/br/com/fiap/tech/challengeiii/fuiegoxxtei/infrastructure/persistence/entity/HorarioDisponiveisEntity.java
@@ -0,0 +1,26 @@
+package br.com.fiap.tech.challengeiii.fuiegoxxtei.infrastructure.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalTime;
+
+@Data
+@Document(collection = "db_horarios_disponiveis")
+@NoArgsConstructor
+@AllArgsConstructor
+public class HorarioDisponiveisEntity {
+
+    @Id
+    private String id;
+    private String mesa;
+    private LocalTime horario;
+
+    @DBRef
+    private RestauranteEntity restaurante;
+
+}
